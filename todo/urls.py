977c7e86eea314f3a23c399a9f56a4dc8bca51fc
LordGameleo <adhikarihrithik@gmail.com>
2018-08-11 17:30:53 +0530

    This commit is just to update a minor change I am working for adding click to expand button

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.dashboard, name = 'dashboard'),
     path('change_task_status/', views.change_task_status, name = 'change_task_status'),
+    path('get/details/', views.get_details, name = 'get_details')
 ]
--- todo/views.py
+++ todo/views.py
@@ -19,13 +19,31 @@ def change_task_status(request):
         status = True
     else:
         status = False
-
+    task_id = "span_task_" + str(task.id)
     task.task_done_flag = status
     task.save()
     data = {
-    'data_saved' : 1 
+    'data_saved' : 1,
+    'span_id' : task_id,
     }
     return JsonResponse(data)
+
+def get_details(request):
+    id = request.GET.get('id', False)
+    if Task.objects.filter(id = id):
+        task = Task.objects.filter(id = id)[0]
+    else:
+        return HttpResponse("No Task Found")
+    detailed_data = {
+    'task_title' : task.task_title,
+    'task_description' : task.task_description,
+    'task_published' : task.task_published,
+    'task_scheduled' : task.task_scheduled,
+    'task_done_flag' : task.task_done_flag,
+    'data_deliverd' : 1,
+    }
+    return JsonResponse(detailed_data)
+
 
 def django_to_html_json(request):
     response_data = {}
